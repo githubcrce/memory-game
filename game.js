afeb3d2824cf7821e50c1793c06f0bfc7e1fafe5
Warren Fernandes <warrenferns01@gmail.com>
2022-10-06 18:39:28 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,29 @@
+//Icons for the game
+
+//Shuffling above array
+
+//Resizing Screen
+
+//Showing instructions
+
+//Starting the game
+function start(r,l) {
+    //Timer and moves
+
+    //Generating item array and shuffling it
+    
+    //Creating table
+    
+    //Hiding instructions screen
+
+}
+
+//Function for flipping blocks
+function change(x) {
+  //Variables
+  
+  //Dont flip for these conditions
+  
+  //Flip
+
+}
