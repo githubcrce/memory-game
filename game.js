1e9a9c6a4f2ca445d19909371c0fcc939f93552b
Martina John <martina07j@gmail.com>
2022-10-07 10:25:55 +0530

    Added icon shuffling function

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 //Icons for the game
 var emojis = ["🎉", "😂", "👽", "🎄", "🎁", "🍓", "💐", "🌵",
        "🌴", "🛺", "⛅","🌈", "☂", "🌊", "😀", "🐤", "👩‍💻", "🎃"];
-       
+
 //Shuffling above array
+function shuffleIcons(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i+1));               
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }   
+  return arr;
+}
+//var arr =shuffleIcons(emojis);
+//console.log(arr)
 
 //Resizing Screen
 
