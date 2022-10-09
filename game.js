f8696f88d6a4f71ef898f89a33416c2bdd2829ab
Martina John <martina07j@gmail.com>
2022-10-09 13:12:37 +0530

    Minor UI changes

--- game.js
+++ game.js
@@ -26,12 +26,12 @@ function init() {
 
 //Showing instructions
 window.onload = function() {
-  $("#ol").html(`<center><div id="inst"><h3>Welcome !</h3>Instructions For Game<br/><br/>
+  $("#ol").html(`<center><div id="inst"><br/><br/><br/><h3>Welcome !</h3>Instructions For Game<br/><br/>
   <li>Make pairs of similiar blocks by flipping them.</li><li>To flip a block you can click on it.</li>
   <li>If two blocks you clicked are not similar, they will be flipped back.</li>
   <p style="font-size:18px;">Click one of the following mode to start the game.</p></div>
   <button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button>
-  <button onclick="start(4, 5)">4 x 5</button><button onclick="start(5, 6)">5 x 6</button>
+  <button onclick="start(4, 5)">4 x 5</button> <button onclick="start(5, 6)">5 x 6</button>
   <button onclick="start(6, 6)">6 x 6</button></center>`);
 }
 
